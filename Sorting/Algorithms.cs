26408542edb25f80887656d5efd08cba0e75e428
Ben Villalobos <villalobosb93@gmail.com>
2017-02-04 09:27:49 -0800

    Initial Project Commit. Insertion Sort completed / commented.

---
+++ Sorting/Algorithms.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Sorting
+{
+    public static class Algorithms
+    {
+        /*  INSERTION SORT
+         *  Time complexity: O(n^2).
+         *  
+         *  Concept:
+         *  Iterate through the array. O(n)
+         *  Compare the current item to every item before it. O(n)
+         *  Place the current item in front of the first item that it is less than. O(1)
+         * */
+        public static int[] InsertionSort(int[] data)
+        {
+            int currentElement = 0;
+
+            //O(n)
+            for (int i = 0, j = 0; i < data.Length; i++) 
+            {
+                //O(1)
+                currentElement = data[i];
+
+                //O(n)
+                //In the worst case, the final item should be the first item.
+                for (j = i; j > 0 && data[j-1] > currentElement; j--) 
+                {
+                    //O(1)
+                    data[j] = data[j - 1];
+                }
+                //O(1)
+                data[j] = currentElement;
+            }
+            return data;
+        }
+        
+        //Just for fun
+        public static void InsertionSortGeneric<T>(T[] data) 
+            where T : IComparable<T>
+        {
+            T currentElement;
+
+            for (int i = 0, j = 0; i < data.Length; i++)
+            {
+                currentElement = data[i];
+                for (j = i; j > 0 && data[j-1].CompareTo(currentElement) > 0; j--)
+                {
+                    data[j] = data[j - 1];
+                }
+                data[j] = currentElement;
+            }
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] reversedArray = { 6, 5, 4, 3, 2, 1 };
+
+            Algorithms.InsertionSort(reversedArray);
+
+            DisplayArray(reversedArray, "Insertion Sort - Reversed Data: ");
+
+            Console.ReadKey();
+        }
+
+        static void DisplayArray<T>(T[] data, string message = "")
+        {
+            Console.Write(message);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(string.Format("{0} ", data[i]));
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------");
+        }
+    }
+}
